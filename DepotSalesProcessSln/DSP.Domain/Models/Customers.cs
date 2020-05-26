3adb502c99c98e2f7f577a75df7a5cefe42ec6f3
AakashAaku <guragaiaakash@gmail.com>
2020-05-26 16:23:37 +0545

    Customer added in domin structure

---
+++ DepotSalesProcessSln/DSP.Domain/Models/Customers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSP.Domain.Models
+{
+    public class Customers
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public long PhoneNumber { get; set; }
+    }
+}
