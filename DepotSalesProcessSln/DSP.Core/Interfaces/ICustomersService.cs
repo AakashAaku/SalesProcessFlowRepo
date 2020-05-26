1804ef524d55f11235b4ea672402daf39a0a4616
AakashAaku <guragaiaakash@gmail.com>
2020-05-26 18:10:40 +0545

    Service and interfaces are implemented for dependency injection

---
+++ DepotSalesProcessSln/DSP.Core/Interfaces/ICustomersService.cs
@@ -0,0 +1,12 @@
+﻿using DSP.Core.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSP.Core.Interfaces
+{
+    public interface ICustomersService
+    {
+        IEnumerable<CustomersDTO> GetAllCustomer();
+    }
+}
---
+++ DepotSalesProcessSln/DSP.Domain/Interfaces/ICustomersRepository.cs
@@ -0,0 +1,12 @@
+﻿using DSP.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSP.Domain.Interfaces
+{
+    public interface ICustomersRepository
+    {
+        IEnumerable<Customers> GetAllCustomers();
+    }
+}
