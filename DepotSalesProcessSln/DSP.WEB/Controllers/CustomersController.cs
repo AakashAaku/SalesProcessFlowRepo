7e9177a9706c7f4f0fdb3dfea164945e07b326d6
AakashAaku <guragaiaakash@gmail.com>
2020-05-27 09:10:29 +0545

    Complete architectcure implemented in Web . Now Lets start the real work

---
+++ DepotSalesProcessSln/DSP.WEB/Controllers/CustomersController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DSP.Core.DTO;
+using DSP.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DSP.WEB.Controllers
+{
+    public class CustomersController : Controller
+    {
+
+        private ICustomersService _customerService;
+
+        public CustomersController(ICustomersService customerService)
+        {
+            _customerService = customerService;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            CustomersDTO customerDto = _customerService.GetAllCustomer();
+            return View(customerDto);
+        }
+    }
+}
